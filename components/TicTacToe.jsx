b2fa928d80dd52c347a2c37c6710fd1688247689
guille <guillerhuguet@gmail.com>
2024-08-26 00:43:49 +0100

    resize table game

--- components/TicTacToe.jsx
+++ components/TicTacToe.jsx
@@ -1,7 +1,8 @@
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 import React, { useState } from "react";
 import confetti from "canvas-confetti";
-import {Card, CardBody} from "@nextui-org/react";
+import { Card, CardBody } from "@nextui-org/react";
+
 function TicTacToe() {
     const [tablegame, setTable] = useState(Array(9).fill(null));
     const [turn, changeTurn] = useState('⭕');
@@ -15,56 +16,57 @@ function TicTacToe() {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6]
-    ]
+    ];
+
     const checkWinner = (board) => {
         let isfull = true;
-        for(const combo of COMBOS) {
+        for (const combo of COMBOS) {
             const [a, b, c] = combo;
-            if (board[a] == turn && board[b] == turn && board[c] == turn){
-                confetti()
-                Swal.fire("El ganador es "+ turn);
-                finishGame(true)
+            if (board[a] === turn && board[b] === turn && board[c] === turn) {
+                confetti();
+                Swal.fire("El ganador es " + turn);
+                finishGame(true);
+                return;
             }
-            
         }
         for (let i = 0; i < board.length; i++) {
-            !board[i] ? isfull = false : '';
+            if (!board[i]) isfull = false;
         }
-        isfull ? Swal.fire("EMPATE") && finishGame(true) : '';
-    }
+        if (isfull) {
+            Swal.fire("EMPATE");
+            finishGame(true);
+        }
+    };
+
     const updateTable = (index) => {
-        if (tablegame[index]) return; // Evita que se cambie una celda ya ocupada
+        if (tablegame[index] || isFinished) return; // Evita que se cambie una celda ya ocupada o si el juego ha terminado
 
         const newBoard = [...tablegame];
         newBoard[index] = turn;
         setTable(newBoard);
         
         changeTurn(turn === '⭕' ? '❌' : '⭕');
-        checkWinner(newBoard)
-        
-    }
+        checkWinner(newBoard);
+    };
 
     return (
-        <main className="tictac">
+        <main className="flex flex-col items-center p-4">
             <div className="tablegame">
                 <section className="game">
                     {tablegame.map((value, i) => (
-                        <div className="cell" key={i} onClick={() => !isFinished? updateTable(i) : ''}>
-                            
+                        <div className="cell grid grid-cols-3" key={i} onClick={() => !isFinished ? updateTable(i) : null}>
                             {value}
                         </div>
                     ))}
                 </section>
             </div>
-            <div className="turns">
-            <Card>
-                <CardBody>
-                    <p>Turno: {turn.toUpperCase()}</p>
-                </CardBody>
-            </Card>
-                
+            <div className="turns mt-4">
+                <Card>
+                    <CardBody>
+                        <p>Turno: {turn.toUpperCase()}</p>
+                    </CardBody>
+                </Card>
             </div>
-            
         </main>
     );
 }
